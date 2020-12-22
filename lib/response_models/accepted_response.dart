8bb31b7cd00fa45deb646014b6cc58868c4a80bb
bakhanets <Bakhanetssimon@gmail.com>
2020-12-22 15:08:58 +0700

    home and accepted responses added

---
+++ lib/response_models/accepted_response.dart
@@ -0,0 +1,13 @@
+class AcceptedResponse {
+  final bool accepted;
+  final String key;
+  final String valueInCrypto;
+  final String date;
+
+  AcceptedResponse(
+    this.accepted,
+    this.key,
+    this.valueInCrypto,
+    this.date,
+  );
+}
---
+++ lib/response_models/home_response.dart
@@ -0,0 +1,11 @@
+class HomeResponse {
+  final String key;
+  final String valueInCrypto;
+  final String valueInDollars;
+
+  HomeResponse(
+    this.key,
+    this.valueInCrypto,
+    this.valueInDollars,
+  );
+}
