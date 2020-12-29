b32a61c4489700ee14724929c546f400a35c5c02
bakhanets <Bakhanetssimon@gmail.com>
2020-12-29 21:53:01 +0700

    storage service is ready

---
+++ lib/services/secure_storage_service/i_secure_storage_service.dart
@@ -0,0 +1,13 @@
+import 'package:ck_login/services/secure_storage_service/core/i_secure_storage.dart';
+
+class ISecureStorageService extends ISecureStorage {
+  final String _pinCodeKey;
+
+  ISecureStorageService(
+    this._pinCodeKey,
+  );
+
+  Future<void> savePinCode(String pinCode) async {
+    await write(_pinCodeKey, pinCode);
+  }
+}
